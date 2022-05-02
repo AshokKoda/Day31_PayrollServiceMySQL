c397ff4f2e756a7126dc45c5cace0503083a775c
AshokKoda <a.koda59@gmail.com>
2022-05-02 18:10:34 +0530

    Added:Refactor few queries

--- payroll_service.sql
+++ payroll_service.sql
@@ -53,7 +53,7 @@ add department varchar(50) not null after salary;
 alter table employee_payroll
 add address varchar(250) default 'Address' after phoneno;
 
-/*--------UC9----------*/
+/*--------UC9 , UC10----------*/
 select * from employee_payroll;
 alter table employee_payroll rename column salary to basic_pay;
 
@@ -67,3 +67,62 @@ update employee_payroll set deduction=(basic_pay*0.1) where id!=0;
 update employee_payroll set taxable_pay=(basic_pay-deduction) where id!=0;
 update employee_payroll set income_tax=(taxable_pay*0.2) where id!=0;
 update employee_payroll set net_pay=(taxable_pay-income_tax) where id!=0;
+
+select * from employee_payroll where name="Pooja";
+
+/*--------UC11, UC12----------*/
+create table department_tbl (
+	dept_id int auto_increment,
+    dept_name varchar(15),
+    dept_desc varchar(30),
+    primary key(dept_id)
+);
+
+select * from department_tbl;
+
+insert into department_tbl (dept_name, dept_desc) 
+values("Sales","Handling sales of company"),
+("Marketing", "marketing department"),
+("IT", "Technical team"),
+("HR","Human Resource"),
+("Account","accounting of company");
+
+alter table employee_payroll drop column department;
+
+create table employee_department (
+	id int auto_increment,
+    emp_id int,
+    dept_id int,
+    primary key(id),
+    foreign key(emp_id) references employee_payroll(id),
+    foreign key(dept_id) references department_tbl(dept_id)
+);
+
+insert into employee_department (emp_id, dept_id) values (3,3);
+insert into employee_department (emp_id, dept_id) values (1,3);
+select * from employee_department;
+
+select ep.id, ep.name, d.dept_name, d.dept_desc from employee_payroll ep, department_tbl d, employee_department ed
+where ep.id=ed.emp_id AND d.dept_id=ed.dept_id;
+
+create table salary_tbl (
+	id int auto_increment,
+    basic_pay double,
+    deduction double,
+    taxable_pay double,
+    tax double,
+    net_pay double,
+    emp_id int,
+    primary key (id),
+    foreign key(emp_id) references employee_payroll(id)
+);
+
+insert into salary_tbl (basic_pay,deduction, taxable_pay, tax, net_pay,emp_id)
+select basic_pay, deduction, taxable_pay, income_tax, net_pay,id from employee_payroll;
+
+select * from salary_tbl;
+
+alter table employee_payroll drop column basic_pay, drop column deduction, 
+drop column taxable_pay, drop column income_tax, drop column net_pay;
+
+select ep.id, ep.name, s.* from employee_payroll ep, salary_tbl s;
